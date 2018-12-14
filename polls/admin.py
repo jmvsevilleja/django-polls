109f14fbc92fbbba62d9aa10c29fac93e7518196
Jess Mark Sevilleja <jessmark.sevilleja@bayviewtechnology.com>
2018-12-14 10:17:40 +0800

    db shell admin views query template url

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
+from .models import Choice
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+
+admin.site.register(Question)
+admin.site.register(Choice)
